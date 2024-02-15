d5fc7508f7b2e523965fd081127144a4e9d2bc59
Ajrapetov <artem.ajrapetov@myq-solution.com>
2024-02-15 18:18:43 +0100

    Second part of 5th day puzzle

--- src/main/kotlin/5.kt
+++ src/main/kotlin/5.kt
@@ -1,11 +1,59 @@
 package org.aoc
 
 import java.io.File
+import java.util.concurrent.ForkJoinPool
 import kotlin.jvm.internal.Ref
+import kotlin.math.min
+import kotlin.streams.asStream
+
 
 object IfYouGiveASeedAFertilizer {
 
     fun first(path: String): Long {
+        val (seeds, pipeline) = buildPipeline(path)
+        println("Mapping seeds...")
+        return seeds.stream().map { seed ->
+            fold(seed, pipeline)
+        }.min(Long::compareTo).get()
+    }
+
+    fun second(path: String): Long {
+        val (seeds, pipeline) = buildPipeline(path)
+        println("Mapping seeds...")
+        return seeds.let {
+            buildList {
+                for (i in it.indices step 2) {
+                    val start = it[i]
+                    val len = it[i + 1]
+                    add(start..<start + len)
+                }
+            }.also { println(it) }
+        }.map { range -> range.asSequence() }.let {
+            var min = Long.MAX_VALUE
+            val pool = ForkJoinPool(14)
+            for (i in it) {
+                min = pool.submit<Long> {
+                    min(i.asStream().parallel().map { seed -> fold(seed, pipeline) }
+                        .min(Long::compareTo).get(), min)
+                }.get()
+            }
+            min
+        }
+    }
+
+    private fun fold(
+        seed: Long,
+        pipeline: List<Map<LongRange, (Long) -> Long>>
+    ): Long {
+        println("Seed: $seed")
+        return pipeline.fold(seed) { key: Long, map: Map<LongRange, (Long) -> Long> ->
+            ((map.keys.firstOrNull() { it.contains(key) }
+                ?.let { range -> map[range] }
+                ?.let { it(key) }) ?: key).also { println("Mapped $key to $it") }
+        }
+    }
+
+    private fun buildPipeline(path: String): Pair<List<Long>, List<Map<LongRange, (Long) -> Long>>> {
         val ptr = Ref.IntRef().apply { element = 0 } // line ptr reference
         val lines = File(path).readLines()
         val seeds = lines[ptr.element++].split(" ").mapNotNull { extractDigits<Long>(it) }
@@ -14,15 +62,7 @@ object IfYouGiveASeedAFertilizer {
                 add(parseMap(lines, ptr) ?: break)
             } while (true)
         }.onEach { println(it) } // parse maps as a chain
-        println("Mapping seeds...")
-        return seeds.stream().map { seed ->
-            println("Seed: $seed")
-            pipeline.fold(seed) { key: Long, map: Map<LongRange, (Long) -> Long> ->
-                ((map.keys.firstOrNull() { it.contains(key) }
-                    ?.let { range -> map[range] }
-                    ?.let { it(key) }) ?: key).also { println("Mapped $key to $it") }
-            }
-        }.min(Long::compareTo).get()
+        return Pair(seeds, pipeline)
     }
 
     fun parseMap(lines: List<String>, ptr: Ref.IntRef): Map<LongRange, (Long) -> Long>? {
@@ -33,8 +73,8 @@ object IfYouGiveASeedAFertilizer {
                 val (destinationRangeStart,
                     sourceRangeStart,
                     rangeLength) = lines[ptr.element].split(" ").mapNotNull { extractDigits<Long>(it) }.toTypedArray()
-                this[sourceRangeStart.rangeUntil(sourceRangeStart + rangeLength)] = {
-                    source: Long -> source + (destinationRangeStart - sourceRangeStart)
+                this[sourceRangeStart.rangeUntil(sourceRangeStart + rangeLength)] = { source: Long ->
+                    source + (destinationRangeStart - sourceRangeStart)
                 }
                 ptr.element++
             }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,6 @@ import kotlin.time.measureTime
 
 fun main() {
     measureTime {
-        println(IfYouGiveASeedAFertilizer.first("src/main/resources/5.txt"))
+        println(IfYouGiveASeedAFertilizer.second("src/main/resources/5.txt"))
     }.also { println("Running time: ${it.toString(DurationUnit.SECONDS, 2)}") }
 }
